91ffc0ef60900e053967b4d16b340f26ed6005e0
wiggin77 <wiggin77@warpmail.net>
2019-09-03 18:29:19 -0400

    fleshing

--- logr.go
+++ logr.go
@@ -1,5 +1,25 @@
 package logr
 
-type Logger struct {
+// Fields type, used to pass to `WithFields`.
+type Fields map[string]interface{}
 
+// LogRec collects raw, unformatted data to be logged.
+type LogRec struct {
+}
+
+// Formatter turns a LogRec into a formatted string.
+type Formatter interface {
+}
+
+// Target represents a destination for log records such as file,
+// database, TCP socket, etc.
+type Target interface {
+}
+
+// Logr provides APIs for configuration and logging.
+type Logr interface {
+}
+
+// Logger implements Logr APIs.
+type Logger struct {
 }
