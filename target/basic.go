db31e878ede4bcc8512fae9b4f5929af89531dfc
wiggin77 <wiggin77@warpmail.net>
2019-10-05 22:30:53 -0400

    refactor basic target

--- target/basic.go
+++ target/basic.go
@@ -13,21 +13,40 @@ type RecordWriter interface {
 }
 
 // Basic provides the basic functionality of a Target that can be used
-// to more easily compose your own Targets.
+// to more easily compose your own Targets. To use, just embed Basic
+// in your target type, implement `RecordWriter`, and call `Start`.
 type Basic struct {
 	target logr.Target
-	in     chan *logr.LogRec
-	done   chan struct{}
-	w      RecordWriter
+
+	filter    logr.Filter
+	formatter logr.Formatter
+
+	in   chan *logr.LogRec
+	done chan struct{}
+	w    RecordWriter
 }
 
 // Start initializes this target helper and starts accepting log records for processing.
-func (b *Basic) Start(target logr.Target, rw RecordWriter, maxQueued int) {
+func (b *Basic) Start(target logr.Target, rw RecordWriter, filter logr.Filter, formatter logr.Formatter, maxQueued int) {
 	b.target = target
+	b.filter = filter
+	b.formatter = formatter
 	b.in = make(chan *logr.LogRec, maxQueued)
 	b.done = make(chan struct{}, 1)
 	b.w = rw
 	go b.start()
+}
+
+// IsLevelEnabled returns true if this target should emit
+// logs for the specified level. Also determines if
+// a stack trace is required.
+func (b *Basic) IsLevelEnabled(lvl logr.Level) (enabled bool, stacktrace bool) {
+	return b.filter.IsEnabled(lvl), b.filter.IsStacktraceEnabled(lvl)
+}
+
+// Formatter returns the Formatter associated with this Target.
+func (b *Basic) Formatter() logr.Formatter {
+	return b.formatter
 }
 
 // Shutdown stops processing log records after making best
--- target/writer.go
+++ target/writer.go
@@ -10,36 +10,15 @@ import (
 // Writer outputs log records to any `io.Writer`.
 type Writer struct {
 	Basic
-	filter    logr.Filter
-	formatter logr.Formatter
-	out       io.Writer
-	mux       sync.Mutex
+	out io.Writer
+	mux sync.Mutex
 }
 
 // NewWriterTarget creates a target capable of outputting log records to an io.Writer.
 func NewWriterTarget(filter logr.Filter, formatter logr.Formatter, out io.Writer, maxQueue int) *Writer {
-	w := &Writer{filter: filter, formatter: formatter, out: out}
-	w.Basic.Start(w, w, maxQueue)
+	w := &Writer{out: out}
+	w.Basic.Start(w, w, filter, formatter, maxQueue)
 	return w
-}
-
-// Shutdown makes best effort to flush target queue and
-// frees/closes all resources.
-func (w *Writer) Shutdown() error {
-	w.Basic.Shutdown()
-	return nil
-}
-
-// IsLevelEnabled returns true if this target should emit
-// logs for the specified level. Also determines if
-// a stack trace is required.
-func (w *Writer) IsLevelEnabled(lvl logr.Level) (enabled bool, stacktrace bool) {
-	return w.filter.IsEnabled(lvl), w.filter.IsStacktraceEnabled(lvl)
-}
-
-// Formatter returns the Formatter associated with this Target.
-func (w *Writer) Formatter() logr.Formatter {
-	return w.formatter
 }
 
 // Write converts the log record to bytes, via the Formatter,
