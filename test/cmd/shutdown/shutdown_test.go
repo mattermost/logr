d8eb13553bad3e11f799399ed4e906fda6332f40
wiggin77 <wiggin77@warpmail.net>
2020-02-12 09:21:38 -0500

    handle Shutdown before AddTarget

---
+++ test/cmd/shutdown/shutdown_test.go
@@ -0,0 +1,19 @@
+package test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/wiggin77/logr"
+)
+
+func TestShutdown_NoTargetsAdded(t *testing.T) {
+	lgr := &logr.Logr{MaxQueueSize: 1000}
+
+	time.Sleep(2 * time.Second)
+
+	err := lgr.Shutdown()
+	if err != nil {
+		t.Error(err)
+	}
+}
