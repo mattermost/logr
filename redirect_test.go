3748e169f4f05fea4c825765c753096b667db615
wiggin77 <wiggin77@warpmail.net>
2020-09-13 22:26:06 -0400

    added RedirectStdLog api

---
+++ redirect_test.go
@@ -0,0 +1,53 @@
+package logr_test
+
+import (
+	"bytes"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/mattermost/logr/v2"
+	"github.com/mattermost/logr/v2/format"
+	"github.com/mattermost/logr/v2/target"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLogr_RedirectStdLog(t *testing.T) {
+	buf := &bytes.Buffer{}
+
+	lgr, err := logr.New(logr.StackFilter("log"))
+	require.NoError(t, err)
+
+	filter := &logr.StdFilter{logr.Info, logr.Error}
+	formatter := &format.Plain{Delim: " | "}
+
+	tgt := target.NewWriterTarget(buf)
+	err = lgr.AddTarget(tgt, "buf", filter, formatter, 1000)
+	require.NoError(t, err)
+
+	tgt = target.NewWriterTarget(os.Stdout)
+	err = lgr.AddTarget(tgt, "stdout", filter, formatter, 1000)
+	require.NoError(t, err)
+
+	// remember old settings.
+	flags := log.Flags()
+	prefix := log.Prefix()
+
+	restoreFunc := lgr.RedirectStdLog()
+
+	log.Println("Peaky Blinders!")
+
+	err = lgr.Flush()
+	require.NoError(t, err)
+
+	require.Contains(t, buf.String(), "Peaky Blinders!")
+
+	err = lgr.Shutdown()
+	require.NoError(t, err)
+
+	restoreFunc()
+
+	// check settings restored
+	require.Equal(t, flags, log.Flags())
+	require.Equal(t, prefix, log.Prefix())
+}
