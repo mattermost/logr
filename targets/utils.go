bf3496ffac6944e40a669d06808b0dd6023e6097
Doug Lauder <wiggin77@warpmail.net>
2025-02-21 13:31:37 -0500

    MM-63161: Support a default cert specified via env var  (#50)
    * add Aider files to .gitignore
    
    * Add a default cert for TLS capable targets that can be specified via env var.

--- targets/utils.go
+++ targets/utils.go
@@ -4,19 +4,32 @@ import (
 	"crypto/x509"
 	"encoding/base64"
 	"errors"
-	"io/ioutil"
+	"os"
 )
 
-// GetCertPool returns a x509.CertPool containing the cert(s)
-// from `cert`, which can be a path to a .pem or .crt file,
-// or a base64 encoded cert.
-func GetCertPool(cert string) (*x509.CertPool, error) {
+const (
+	DefaultCertKey = "LOGR_DEFAULT_CERT"
+)
+
+// GetCertPoolOrNil returns a x509.CertPool containing the cert(s) from `cert`,
+// or from the certs specified by the env var `LOGR_DEFAULT_CERT`, either of which
+// can be a path to a .pem or .crt file, or a base64 encoded cert.
+//
+// If a cert is specified by either `cert` or `LOGR_DEFAULT_CERT`, but the cert
+// is invalid then an error is returned.
+//
+// If no certs are specified by either `cert` or `LOGR_DEFAULT_CERT`, then
+// nil is returned.
+func GetCertPoolOrNil(cert string) (*x509.CertPool, error) {
 	if cert == "" {
-		return nil, errors.New("no cert provided")
+		cert = getDefaultCert()
+		if cert == "" {
+			return nil, nil // no cert provided, not an error but no pool returned
+		}
 	}
 
 	// first treat as a file and try to read.
-	serverCert, err := ioutil.ReadFile(cert)
+	serverCert, err := os.ReadFile(cert)
 	if err != nil {
 		// maybe it's a base64 encoded cert
 		serverCert, err = base64.StdEncoding.DecodeString(cert)
@@ -30,4 +43,8 @@ func GetCertPool(cert string) (*x509.CertPool, error) {
 		return pool, nil
 	}
 	return nil, errors.New("cannot parse cert")
+}
+
+func getDefaultCert() string {
+	return os.Getenv(DefaultCertKey)
 }
